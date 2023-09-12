fe6f48a7a409d9896564903c570035f4fa26c338
vboxuser <khaoulaaourrath@gmail.com>
2023-09-12 13:46:31 +0200

    Completed assignment

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+// Add the .hidden class to the error modal initially
+const errorModal = document.getElementById("modal");
+errorModal.className = "hidden";
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      }
+    })
+    .catch(function(error) {
+      // Display the error modal and message
+      errorModal.classList.remove("hidden");
+      errorModal.innerText = error;
+      
+      // Use setTimeout to hide the modal after 3 seconds
+      setTimeout(() =>  errorModal.classList.add("hidden"), 3000);
+    });
+}
 
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
